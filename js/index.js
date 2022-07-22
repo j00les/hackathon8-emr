cee72bdae95fbf045ea46357cf1d3e7008355731
Mickyokta <micky.okta36@gmail.com>
2022-07-22 13:32:29 +0700

    index dan html manajemen

--- js/index.js
+++ js/index.js
@@ -1,82 +1,83 @@
 //bakal dirender di table row
 let namaPasienData = [
-  {
+    {
     namaPasien: "Nina",
-    idRN: "001",
-    tempatLahir: "Jakarta",
+    idRN: 1,
     tanggalLahir: "27 Mei 1989",
     jenisKelamin: "perempuan",
-    noTelepon: "0812343210",
     email: "nina123@gmail.com",
-    namaDokter: "Dr Frans",
-    namaKlinik: "Hackti Bunda Sejahtera",
-    tanggalPerawatan: "20 Juli 2022",
-    namaPerawatan: "rabies",
+    namaPerawatan: "Scaling",
   },
   {
     namaPasien: "Nino",
-    idRN: "002",
-    tempatLahir: "Medan",
+    idRN: 2,
     tanggalLahir: "27 Juli 1919",
     jenisKelamin: "laki-laki",
-    noTelepon: "08123432102",
     email: "nino123@gmail.com",
-    namaDokter: "Dr Feri",
-    namaKlinik: "Hackti Bunda Sejahtera",
-    tanggalPerawatan: "19 Juli 2022",
-    namaPerawatan: "kanker",
+    namaPerawatan: "Bleaching",
   },
   {
     namaPasien: "Antoni",
-    idRN: "003",
-    tempatLahir: "Palembang",
+    idRN: 3,
     tanggalLahir: "27 September 1993",
     jenisKelamin: "laki-laki",
-    noTelepon: "0818213582102",
     email: "antoniii@gmail.com",
-    namaDokter: "Dr Feri",
-    namaKlinik: "Hackti Bunda Sejahtera",
-    tanggalPerawatan: "28 Juni 2022",
-    namaPerawatan: "flu burung",
+    namaPerawatan: "Tumpatan gigi",
   },
   {
     namaPasien: "Jonathan",
-    idRN: "004",
-    tempatLahir: "Medan",
+    idRN: 4,
     tanggalLahir: "27 Juli 1919",
     jenisKelamin: "laki-laki",
-    noTelepon: "081237273602",
     email: "jonathandx@gmail.com",
-    namaDokter: "Dr Frans",
-    namaKlinik: "Hackti Bunda Sejahtera",
-    tanggalPerawatan: "29 Mei 2022",
-    namaPerawatan: "batuk berdahak",
+    namaPerawatan: "Veneer",
   },
   {
     namaPasien: "Adinda",
-    idRN: "005",
-    tempatLahir: "Yogyakarta",
+    idRN: 5,
     tanggalLahir: "13 Agustus 2000",
     jenisKelamin: "perempuan",
-    noTelepon: "08184573204",
     email: "adinda92@gmail.com",
-    namaDokter: "Dr Frans",
-    namaKlinik: "Hackti Bunda Sejahtera",
-    tanggalPerawatan: "17 Juli 2022",
-    namaPerawatan: "antrax",
+    namaPerawatan: "Implan gigi",
   },
 ];
+let namaInput = document.querySelector('#nama')
+let namaPerawatanInput = document.querySelector('#nama-perawatan')
+let tanggalLahirInput = document.querySelector('#tanggal-lahir')
+let jenisKelaminInput = document.querySelector('#jenis-kelamin')
+let emailInput = document.querySelector('#email')
 
-//output object
+let submitButton = document.querySelector('#btn-submit')
+submitButton.addEventListener('click', function(e){
+    e.preventDefault()
+    let inputPasien = {
+        namaPasien: namaInput.value,
+        idRN: namaPasienData.length + 1,
+        namaPerawatan: namaPerawatanInput.value,
+        tanggalLahir: tanggalLahirInput.value,
+        jenisKelamin: jenisKelaminInput.value,
+        email: emailInput.value
+    }
+    console.log(inputPasien)
+    namaPasienData.push(inputPasien)
+    adminShow()
+})
+adminShow()
+function adminShow(){
 let tableWrapper = document.querySelector(".table_wrapper");
+// let createTr = document.createElement("tr");
+tableWrapper.innerHTML = ''
 for (let i in namaPasienData) {
-  let createTr = document.createElement("tr");
-  createTr.innerHTML = `
+//   let createTr = document.createElement("tr");
+  tableWrapper.innerHTML += `<tr>
                 <td>
                     <p>${namaPasienData[i].idRN}</p>
                 </td>
                 <td>
                     <p>${namaPasienData[i].namaPasien}</p>
+                </td>
+                <td>
+                    <p>${namaPasienData[i].namaPerawatan}</p>
                 </td>
                 <td>
                     <p>${namaPasienData[i].tanggalLahir}</p>
@@ -87,7 +88,27 @@ for (let i in namaPasienData) {
                 <td>
                     <p>${namaPasienData[i].email}</p>
                 </td>
+                <td>
+                    <a id="${namaPasienData[i].idRN}" 
+                    data-toggle="tooltip" 
+                    title="Delete" 
+                    class="btn btn-xs btn-danger"
+                     onclick='adminDelete(${namaPasienData[i].idRN})'>
+                     <i class="fa fa-times"></i>
+                    </a>
+                </td>
+                </tr>
                 
     `;
-  tableWrapper.appendChild(createTr);
+//   tableWrapper.appendChild(createTr);
+}
+}
+
+function adminDelete(id){
+    for(let i = 0; i < namaPasienData.length; i++){
+        if(id === namaPasienData[i].idRN){
+            namaPasienData.splice(i, 1)
+        }
+    }
+    adminShow()
 }
